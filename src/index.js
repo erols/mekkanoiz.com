affe24b9f4ba51432b7424e3109e483dc5432c06
Erol Suleyman <erol.suleyman@gmail.com>
2017-04-18 23:32:31 +0200

    made tests pass

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import {render} from 'react-dom'
 import App from './App'
 
 render(<App />, document.querySelector('main'))
+
+var exports = module.exports ={};
+
+exports.React = React;
+exports.render = render;
+exports.App = App;
